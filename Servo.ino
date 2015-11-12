5849ad0580eed4fa62d99e2884782833a90c9a7f
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-11-12 21:32:23 +0000

    Set servo position

--- Servo.ino
+++ Servo.ino
@@ -1,33 +1,16 @@
 #include <Servo.h>
 
 Servo servo;
-unsigned long t0;
-int pulse;
-char dir;
 
 void setup() {
   servo.attach(6);
-  t0 = millis();
-  pulse = 0;
-  dir = 1;
 }
 
 void loop() {
-  // use writeMicroseconds, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH
-  int dt = millis() - t0;
-  t0 += dt;
-  if (dir == 1) {
-    pulse += dt;
-    if (pulse > MAX_PULSE_WIDTH) {
-      pulse = MAX_PULSE_WIDTH;
-      dir = 0;
-    }
-  } else {
-    pulse -= dt;
-    if (pulse < MIN_PULSE_WIDTH) {
-      pulse = MIN_PULSE_WIDTH;
-      dir = 1;
-    }
-  };
-  servo.writeMicroseconds(pulse);
+  servo.write(0);
+  delay(1000);
+  servo.write(180);
+  delay(1000);
+  servo.write(90);
+  delay(10000);
 }
